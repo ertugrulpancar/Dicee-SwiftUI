16fe6b188f0e95918b5dede52bada678f83da67a
Ertuğrul Pancar <21220030023@mersin.edu.tr>
2024-09-04 10:36:51 +0300

    Initial Commit

---
+++ DiceeUI/DiceeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeUIApp.swift
+//  DiceeUI
+//
+//  Created by Ertuğrul Pancar on 4.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
